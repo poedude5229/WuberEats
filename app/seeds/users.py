3368e4ee3959c6e7bb60b48f666a196ff6307d15
Joseph <poedude5229@gmail.com>
2024-05-17 13:21:11 -0400

    Worked up to deploy

---
+++ app/seeds/users.py
@@ -0,0 +1,35 @@
+from app.models import db, User, environment, SCHEMA
+from sqlalchemy.sql import text
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_users():
+    demo = User(
+        username='Joe', firstname="Joe", lastname="Mama", email='Joe@aa.io', role="Joe", password='password')
+    veronica = User(
+        username='Veronica', firstname="Veronica", lastname="Flatto", email='veronica@aa.io', role="VeronicaRole", password='password')
+    andres = User(
+        username='andres', firstname="Andres", lastname="Mercado", email='andres@aa.io',  role="MrKissMeOverThePhone",   password='password')
+    sleepyzachbiden = User(
+        username='zach', firstname="Sick", lastname="Lad", email='sleepboy@aa.io', role="AlwaysSickDuringProjectWeek", password='takesomevitamins'
+    )
+    db.session.add(demo)
+    db.session.add(veronica)
+    db.session.add(andres)
+    db.session.add(sleepyzachbiden)
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_users():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM users"))
+
+    db.session.commit()
