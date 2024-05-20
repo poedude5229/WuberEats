62dcbf26e46285bf3594024086f43243d113ec59
Andres Mercado <andresmercado96@gmail.com>
2024-05-20 13:26:05 -0700

    basicalllllyy finished routes

---
+++ app/forms/menu_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,IntegerField,BooleanField,SelectField
+from wtforms.validators import DataRequired
+from app.models import Menu
+
+class MenuForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    description = StringField("Description", validators=[DataRequired()])
+    price = IntegerField("Price", validators=[DataRequired()])
+    category = SelectField("Category", choices=[("Main Courses", "Main Courses"),("Appetizers", "Appetizers"),("Sides", "Sides"),("Beverages", "Beverages"),("Breakfast", "Breakfast"),("Entrees","Entrees")])
+    is_available = BooleanField("Is available")
+    image_url = StringField("Image url", validators=[DataRequired()])
