ace8cbc51cc1ef7cd83aca14f6e4175cd7bcfed1
Joseph <poedude5229@gmail.com>
2024-05-21 12:25:59 -0400

    Homepage awaiting thunks

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,9 +1,14 @@
 import { NavLink, useNavigate } from "react-router-dom";
+import { RiArrowDropDownLine } from "react-icons/ri";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 import DarkSiteLogo from "../../../public/hamberderDarkLogo.png";
+import magnifyingglasssolid from "../../../public/magnifying-glass-solid.png";
+import { useSelector } from "react-redux";
 function Navigation() {
   let navigate = useNavigate();
+  let user = useSelector((state) => state.session.user);
+  // console.log(user);
   return (
     <nav id="sitenav">
       <div
@@ -25,7 +30,33 @@ function Navigation() {
           WuberEats
         </NavLink>
       </div>
-
+      <div id="address-box">
+        {user?.address} • Now
+        <RiArrowDropDownLine />
+      </div>
+      <div id="searchbox">
+        <input
+          id="wubesearch"
+          style={{
+            backgroundColor: "rgb(227,227,227)",
+            border: "none",
+            marginLeft: "4px",
+            fontFamily: "Inter",
+          }}
+          type="text"
+          placeholder="Wube you some food"
+        />
+        <img
+          style={{
+            width: "25px",
+            height: "25px",
+            marginRight: "15px",
+            cursor: "pointer",
+          }}
+          src={magnifyingglasssolid}
+          alt="The search icon"
+        />
+      </div>
       <div>
         <ProfileButton />
       </div>
