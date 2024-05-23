39d83063a0696ec544334849b98404f73b5bcf1d
Joseph <poedude5229@gmail.com>
2024-05-23 16:55:39 -0400

    Added review box in restaurant detail component

--- react-vite/src/components/Details/RestaurantDetails.jsx
+++ react-vite/src/components/Details/RestaurantDetails.jsx
@@ -4,9 +4,11 @@ import { useParams } from "react-router-dom";
 import { loadRestaurantsThunk } from "../../redux/restaurant";
 import { addToCartThunk } from "../../redux/cart";
 import { MdStarBorder } from "react-icons/md";
+import { FaStar } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import OpenModalMenuItem from "../Navigation/OpenModalMenuItem";
-import DeleteRestaurantModal from "../RestaurantForm/DeleteRestaurant"
+import DeleteRestaurantModal from "../RestaurantForm/DeleteRestaurant";
 import "./Details.css";
 function Details() {
   let { restaurantId } = useParams();
@@ -15,13 +17,13 @@ function Details() {
     dispatch(loadRestaurantsThunk());
   }, [dispatch]);
   let all = useSelector((state) => state.restaurantReducer);
-  const currentUser = useSelector((state => state.session.user))
-  console.log('current-user', currentUser);
+  const currentUser = useSelector((state) => state.session.user);
+  // console.log("current-user", currentUser);
   let cartState = useSelector((state) => {
     state.cart;
   });
   let selected = all[restaurantId];
-  console.log('sel',selected?.id);
+  console.log("sel", selected?.reviews);
   // console.log(selected?.owner_id);
   let menu = selected?.menu_items;
   //   console.log(selected);
@@ -37,17 +39,52 @@ function Details() {
           {selected?.name} &#40;{selected?.address}&#41;{" "}
         </h1>
         <div className="description">{selected?.description}</div>
-        <p className="starRating">
-          {selected?.reviews?.length > 0 ? `${selected?.avgrating}` : `New!`}{" "}
-          {selected?.reviews.length > 0 && <MdStarBorder />}{" "}
-          {selected?.reviews?.length > 0
-            ? selected.reviews.length > 1
-              ? `${selected.reviews.length} reviews`
-              : "1 review"
-            : "Be the first to leave a review!"}
-        </p>
-        <br />
-        
+        <div>
+          <p className="starRating">
+            {selected?.reviews?.length > 0 ? `${selected?.avgrating}` : `New!`}{" "}
+            {selected?.reviews.length > 0 && <MdStarBorder />}{" "}
+            {selected?.reviews?.length > 0
+              ? selected?.reviews?.length > 1
+                ? `${selected?.reviews?.length} reviews`
+                : "1 review"
+              : "Be the first to leave a review!"}
+          </p>
+          <br />
+          {selected?.reviews?.length > 0 && (
+            <div
+              style={{
+                border: "1px solid black",
+                width: "800px",
+                borderRadius: "12px",
+                paddingLeft: "8px",
+              }}
+            >
+              <h3 style={{ cursor: "pointer" }}>Reviews and Ratings</h3>
+              {selected?.reviews?.map((review) => (
+                <>
+                  <span>
+                    <span style={{ fontWeight: "900" }}>
+                      {review.user_firstname}
+                    </span>{" "}
+                    {`${review.rating} stars`}{" "}
+                    {review.rating == 5 ? (
+                      <FaStar style={{ color: "gold" }} />
+                    ) : review.rating == 4 ? (
+                      <FaStar style={{ color: "brown" }} />
+                    ) : review.rating == 3 ? (
+                      <FaStar style={{ color: "grey" }} />
+                    ) : review.rating == 2 ? (
+                      <FaStar />
+                    ) : (
+                      <FaRegStar />
+                    )}
+                  </span>
+                  <p>{review.review}</p>
+                </>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
       <section>
         {menu?.map((item) => (
@@ -80,13 +117,25 @@ function Details() {
 
             <p className="menu-item-price">${item.price}</p>
           </div>
-
         ))}
-        {currentUser.id === selected?.owner_id && <button><NavLink to={`/restaurants/${selected?.id}/update`}>Update Your Restaurant</NavLink></button>}
-        
+        {currentUser.id === selected?.owner_id && (
+          <button>
+            <NavLink to={`/restaurants/${selected?.id}/update`}>
+              Update Your Restaurant
+            </NavLink>
+          </button>
+        )}
+
         <div className="delete-button-container">
-        {currentUser.id === selected?.owner_id && <OpenModalMenuItem itemText={<button>Delete</button>} className='delete-btn-getall' modalComponent={<DeleteRestaurantModal restaurantId={+selected?.id} />}/>}
-        
+          {currentUser.id === selected?.owner_id && (
+            <OpenModalMenuItem
+              itemText={<button>Delete</button>}
+              className="delete-btn-getall"
+              modalComponent={
+                <DeleteRestaurantModal restaurantId={+selected?.id} />
+              }
+            />
+          )}
         </div>
       </section>
     </>
